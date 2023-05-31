19ab7c09ae5dd98ddfb084f6965a7eb377427fe2
rishi <rishi.123kiran@gmail.com>
2023-05-31 15:57:43 -0400

    first

---
+++ Portfoliio-main/src/App.jsx
@@ -0,0 +1,14 @@
+import Sidenav from "./components/Sidenav"
+import Main from "./components/Main"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Sidenav/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
